2d3ebbe8382cc19a79cb766def51f0a554f552ac
akash <160629042+akashsheridan@users.noreply.github.com>
2024-04-09 11:38:01 -0400

    Add files via upload
    Akashdeep

---
+++ src/__init__.py
@@ -0,0 +1,13 @@
+"""
+Employee Management System Module
+
+This module provides various functionalities for managing employee data.
+"""
+# Define the list of modules to be imported when the package is imported
+__all__ = ['employee_operations', 'file_operations', 'menu', 'report_generation']
+
+# Import modules to make them available when the package is imported
+import employee_operations
+import file_operations
+import menu
+import report_generation
